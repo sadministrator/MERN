1ea957a5ab039e7adef7b826bfb4668dc2870488
marco <marco@marcoserrano.com>
2018-11-23 00:32:35 -0600

    Compilation again. *snot bubble*

--- static/App.js
+++ static/App.js
@@ -4,8 +4,8 @@ var contentNode = document.getElementById('contents');
 
 var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
 var message = continents.map(function (c) {
-  return 'Hello ' + c + '!<br/>';
-}); //.join(' ');
+  return 'Hello ' + c + '!';
+}).join(' ');
 
 var component = React.createElement(
   'p',
