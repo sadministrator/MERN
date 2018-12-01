30e99ccfc1a580d1fe554af0040a29d1ed31c4bb
marco <marco@marcoserrano.com>
2018-11-30 23:05:39 -0600

    Created various classes and dynamically composed some components.
    The main changes made were the creation of the IssueList, Issuetable, and
    Issue Row classes with simple structures (IssueAdd and IssueFilter skeletons
    were also created). A global array was also introduced to simulate an
    array retrieved from a server. The array was passed from IssueList to
    IssueTable where the objects in the array were mapped to an array of
    IssueRow instances. The components were nested and then rendered.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,86 @@
 const contentNode = document.getElementById('contents');
 
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
+const issues = [
+  {
+    id: 1, status: 'Open', owner: 'Ravan', created: new Date('2016-08-15'), effort: 5, completionDate: undefined, title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie', created: new Date('2016-08-15'), effort: 14, completionDate: new Date('2016-08-30'), title: 'Missing bottom border on panel',
+  },
+];
 
-const component = <p>{message}</p>; // A simple JSX component
-ReactDOM.render(component, contentNode); // Render the component inside the content Node
+class IssueFilter extends React.Component {
+  render() {
+    return(
+      <div>This is a placeholder for the IssueFilter</div>
+    );
+  }
+}
+
+class IssueRow extends React.Component {
+  render() {
+    //const borderedStyle = {border: "1px solid silver", padding: 4};
+
+    const issue = this.props.issue;
+
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.completionDate ? issue.completionDate.toDateString() : 'Pending'}</td>
+        <td>{issue.title}</td>
+      </tr>
+    );
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+
+    return(
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Completed</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
+    );
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the IssueAdd</div>
+    );
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr/>
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);  //  Render the component inside the content Node
